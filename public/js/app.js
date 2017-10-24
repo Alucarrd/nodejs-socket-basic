8780d6acaf7895564df58dcce44e464eb4453398
Peter Tan <alucarrd@gmail.com>
2017-10-23 22:50:40 -0700

    added custom message event

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,9 @@ var socket = io();
 socket.on('connect', function(){
 	console.log('connected to socket io server now');
 });
+
+
+socket.on('message', function(message){
+	console.log('new message');
+	console.log(message.text);
+});
--- server.js
+++ server.js
@@ -18,7 +18,20 @@ var io = require('socket.io')(http); //this is the format socket.io expect
 app.use(express.static(__dirname + '/public'));
 
 //on will let you listen for event, now it's listening to connection event
-io.on('connection', function(){
+io.on('connection', function(socket){
+	//we get access to individual socket on the server 
+	socket.on('message', function(message){
+		console.log('message received:' + message.text);
+
+		//now to send out the msage
+		//broadcast.emit will send this to everyone but the sender
+		socket.broadcast.emit('message', message);
+	}); //this is now making the server listening to socket msg
+	socket.emit('message', {
+		text : 'Welcome to the chat'
+	}); //emit an event, which takes one parameter value.  So its' better to pass in a javascript object
+
+
 	console.log('user connected via socket.io');
 });
 
